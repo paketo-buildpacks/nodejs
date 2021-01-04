eca96e306eb5442722b3afeec478ad4672928604
Frankie Gallina-Jones <frankieg@vmware.com>
2021-01-04 20:44:32 +0000

    adds procfile to order groups
    Signed-off-by: Josh Zarrabi <jzarrabi@vmware.com>

--- integration/npm_test.go
+++ integration/npm_test.go
@@ -3,7 +3,9 @@ package integration_test
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 	"path/filepath"
 	"testing"
 
@@ -33,12 +35,15 @@ func testNPM(t *testing.T, context spec.G, it spec.S) {
 			image     occam.Image
 			container occam.Container
 
-			name string
+			name   string
+			source string
 		)
 
 		it.Before(func() {
 			var err error
 			name, err = occam.RandomName()
+			Expect(err).NotTo(HaveOccurred())
+			source, err = occam.Source(filepath.Join("testdata", "npm"))
 			Expect(err).NotTo(HaveOccurred())
 		})
 
@@ -46,6 +51,7 @@ func testNPM(t *testing.T, context spec.G, it spec.S) {
 			Expect(docker.Container.Remove.Execute(container.ID)).To(Succeed())
 			Expect(docker.Image.Remove.Execute(image.ID)).To(Succeed())
 			Expect(docker.Volume.Remove.Execute(occam.CacheVolumeNames(name))).To(Succeed())
+			Expect(os.RemoveAll(source)).To(Succeed())
 		})
 
 		it("builds a working OCI image for a simple app", func() {
@@ -54,12 +60,13 @@ func testNPM(t *testing.T, context spec.G, it spec.S) {
 			image, logs, err = pack.WithNoColor().Build.
 				WithBuildpacks(nodeBuildpack).
 				WithPullPolicy("never").
-				Execute(name, filepath.Join("testdata", "npm"))
+				Execute(name, source)
 			Expect(err).NotTo(HaveOccurred())
 
 			Expect(logs).To(ContainLines(ContainSubstring("Node Engine Buildpack")))
 			Expect(logs).To(ContainLines(ContainSubstring("NPM Install Buildpack")))
 			Expect(logs).To(ContainLines(ContainSubstring("NPM Start Buildpack")))
+			Expect(logs).NotTo(ContainLines(ContainSubstring("Procfile Buildpack")))
 
 			container, err = docker.Container.Run.
 				WithEnv(map[string]string{"PORT": "8080"}).
@@ -82,6 +89,49 @@ func testNPM(t *testing.T, context spec.G, it spec.S) {
 			Expect(env.NpmConfigLoglevel).To(Equal("error"))
 			Expect(env.NpmConfigProduction).To(Equal("true"))
 
+		})
+
+		context("when there is a Procfile", func() {
+			it.Before(func() {
+				Expect(ioutil.WriteFile(filepath.Join(source, "Procfile"), []byte("web: node server.js"), 0644)).To(Succeed())
+			})
+
+			it("builds a working OCI image for a simple app and uses the Procfile start command", func() {
+				var err error
+				var logs fmt.Stringer
+				image, logs, err = pack.WithNoColor().Build.
+					WithBuildpacks(nodeBuildpack).
+					WithPullPolicy("never").
+					Execute(name, source)
+				Expect(err).NotTo(HaveOccurred())
+
+				Expect(logs).To(ContainLines(ContainSubstring("Node Engine Buildpack")))
+				Expect(logs).To(ContainLines(ContainSubstring("NPM Install Buildpack")))
+				Expect(logs).To(ContainLines(ContainSubstring("NPM Start Buildpack")))
+				Expect(logs).To(ContainLines(ContainSubstring("Procfile Buildpack")))
+				Expect(logs).To(ContainLines(ContainSubstring("web: node server.js")))
+
+				container, err = docker.Container.Run.
+					WithEnv(map[string]string{"PORT": "8080"}).
+					WithPublish("8080").
+					WithPublishAll().
+					Execute(image.ID)
+				Expect(err).NotTo(HaveOccurred())
+
+				Eventually(container, "5s").Should(BeAvailable())
+
+				response, err := http.Get(fmt.Sprintf("http://localhost:%s/env", container.HostPort("8080")))
+				Expect(err).NotTo(HaveOccurred())
+				Expect(response.StatusCode).To(Equal(http.StatusOK))
+
+				var env struct {
+					NpmConfigLoglevel   string `json:"NPM_CONFIG_LOGLEVEL"`
+					NpmConfigProduction string `json:"NPM_CONFIG_PRODUCTION"`
+				}
+				Expect(json.NewDecoder(response.Body).Decode(&env)).To(Succeed())
+				Expect(env.NpmConfigLoglevel).To(Equal("error"))
+				Expect(env.NpmConfigProduction).To(Equal("true"))
+			})
 		})
 	})
 }
