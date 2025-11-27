c4dc3f7bdd858399d43e967280dc73743152c2d7
Costas Papastathis <papastathiscr@gmail.com>
2025-11-27 13:19:20 +0100

    fix: linting issues

--- integration/node_start_test.go
+++ integration/node_start_test.go
@@ -261,7 +261,9 @@ func testNodeStart(t *testing.T, context spec.G, it spec.S) {
 					response, err = client.Do(request)
 					return err
 				}).Should(BeNil())
-				defer response.Body.Close()
+				defer func() {
+					Expect(response.Body.Close()).To(Succeed())
+				}()
 
 				Expect(response.StatusCode).To(Equal(http.StatusOK))
 
