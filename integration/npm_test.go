3f750da0ab5366b032afc618a99f07404de80b1a
Costas Papastathis <papastathiscr@gmail.com>
2025-11-27 13:19:20 +0100

    fix: linting issues

--- integration/npm_test.go
+++ integration/npm_test.go
@@ -376,7 +376,9 @@ func testNPM(t *testing.T, context spec.G, it spec.S) {
 					response, err = client.Do(request)
 					return err
 				}).Should(BeNil())
-				defer response.Body.Close()
+				defer func() {
+					Expect(response.Body.Close()).To(Succeed())
+				}()
 
 				Expect(response.StatusCode).To(Equal(http.StatusOK))
 
--- integration/yarn_test.go
+++ integration/yarn_test.go
@@ -352,7 +352,9 @@ func testYarn(t *testing.T, context spec.G, it spec.S) {
 					response, err = client.Do(request)
 					return err
 				}).Should(BeNil())
-				defer response.Body.Close()
+				defer func() {
+					Expect(response.Body.Close()).To(Succeed())
+				}()
 
 				Expect(response.StatusCode).To(Equal(http.StatusOK))
 
